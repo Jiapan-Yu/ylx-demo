4910a27078243713a7176b9e3d74c090d5aef82f
Jiapan-Yu <2541771811yjp@gmail.com>
2018-10-29 16:12:46 +0800

    add data to ticket list page from server

--- src/pages/Ticket/TicketList/index.js
+++ src/pages/Ticket/TicketList/index.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
 import TicketItem from './TicketItem'
+import { queryTicketList } from '../../../api/ticket'
 
 export default class TicketList extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: []
+		}
+	}
+
+	componentDidMount() {
+		document.title = '门票列表'
+		queryTicketList().then(res => {
+			const { data } = res.data
+			this.setState({
+				data
+			})
+		})
+	}
+
 	render() {
+		const { data } = this.state
 		return (
 			<div>
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
-				<TicketItem />
+				{
+					data.map(item => (
+						<TicketItem key={item.viewId} bean={item}/>
+					))
+				}
 			</div>
 		)
 	}
