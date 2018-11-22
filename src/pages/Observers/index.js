65c587fa8330d394a18097dfd3b674d5182ce75d
Yu Jiapan <2541771811yjp@gmail.com>
2018-11-22 14:24:50 +0800

    mobx + react part II: computed values

--- src/pages/Observers/index.js
+++ src/pages/Observers/index.js
@@ -5,7 +5,26 @@ import { observer, inject } from 'mobx-react'
 @inject('todoStore')
 @observer
 export default class TodoList extends React.Component {
+  createNew(e) {
+    if(e.which === 13) {
+      this.props.todoStore.createTodo(e.target.value)
+      e.target.value = ''
+    }
+  }
+  filter(e) {
+    this.props.todoStore.filter = e.target.value
+  }
   render() {
-    return <h1>{this.props.todoStore.todos[1]}</h1>
+    const { filter, filteredTodos } = this.props.todoStore
+
+    const todoLis = filteredTodos.map(todo => (
+      <li>{todo}</li>
+    ))
+    return <div>
+      <h1>todos</h1>
+      <input className="create" onKeyPress={this.createNew.bind(this)} />
+      <input className='filter' value={filter} onChange={this.filter.bind(this)} />
+      <ul>{todoLis}</ul>
+    </div>
   }
 }
--- src/pages/Observers/todoStore.js
+++ src/pages/Observers/todoStore.js
@@ -1,8 +1,16 @@
-import { observable } from 'mobx'
+import { computed, observable } from 'mobx'
 
 class TodoStore {
   @observable todos = ['buy milk', 'buy eggs']
   @observable filter = ''
+  @computed get filteredTodos() {// 'computed' will fire very lazily whenever it's needed to fire
+    let matchesFilter = new RegExp(this.filter, 'i')
+    return this.todos.filter(todo => !this.filter || matchesFilter.test(todo))
+  }
+
+  createTodo(value) {
+    this.todos.push(value)
+  }
 }
 
 let todoStore = window.store = new TodoStore
