5e25c00f6d57595a04ca9499d9db49d9765ffb9d
unknown <2541771811yjp@gmail.com>
2019-01-09 18:40:21 +0800

    computed observable with asynchronous support

--- src/pages/MobX/index.js
+++ src/pages/MobX/index.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react'
-import { observable, autorun, action } from 'mobx'
+import { observable, computed, autorun, action } from 'mobx'
 
-let cart = observable({
-  itemCount: 0,
-  items: [],
-  modified: new Date(),
+class Cart {
+  @observable itemCount = 0;
+  @observable.shallow items = [];
+  @observable modified = new Date();
 
-  get description() {
+  @computed get description() {
     switch (this.items.length) {
       case 0:
-        return 'There are no items in the cart'
+        return `There are no items in the cart ${this.modified}`
       case 1:
-        return 'There is one item in the cart'
+        return `There is one item in the cart ${this.modified}`
       default:
-        return `There are ${this.items.length} items in the cart`
+        return `There are ${this.items.length} items in the cart ${this.modified}`
     }
   }
-})
+}
+
+let cart = new Cart()
 
 autorun(() => {
-  console.log(`${cart.description}`)
+  console.log(`${cart.description} ${cart.modified}`)
 })
 
+// Create the actions
 const incrementCount = action(() => {
   cart.items.push(cart.itemCount++)
 })
+
+const addItem = action((name, quantity) => {
+  const item = cart.items.find(x => x.name === name)
+  if (item) {
+    item.quantity += 1
+  } else {
+    cart.items.push({ name, quantity })
+  }
+
+  cart.modified = new Date()
+})
+
+const removeItem = action(name => {
+  const item = cart.items.find(x => x.name === name)
+  if (item) {
+    item.quantity -= 1;
+
+    if (item.quantity <= 0) {
+      cart.items.removeItem(item)
+    }
+
+    cart.modified = new Date()
+  }
+})
+
+// Invoke actions
+console.log("Before timeout: " + new Date())
+setTimeout(addItem, 1000, 'balloons', 2)
+setTimeout(addItem, 2000, 'paint', 2)
+setTimeout(removeItem, 3000, 'paint')
 
 class MobX extends Component {
   state = {
@@ -32,7 +65,7 @@ class MobX extends Component {
   }
 
   componentDidMount() {
-    setInterval(incrementCount, 1000)
+    // setInterval(incrementCount, 1000)
   }
   render() {
     return (
