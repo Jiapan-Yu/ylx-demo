b5cea6ecb55b0890cb50373de213ae31e5a50243
unknown <2541771811yjp@gmail.com>
2019-01-11 11:08:30 +0800

    scope and asynchronous execution in mobx folder

--- src/pages/MobX/index.js
+++ src/pages/MobX/index.js
@@ -1,63 +1,17 @@
 import React, { Component } from 'react'
 import { observable, computed, autorun, action } from 'mobx'
 
-class Cart {
-  @observable itemCount = 0;
-  @observable.shallow items = [];
-  @observable modified = new Date();
-
-  @computed get description() {
-    switch (this.items.length) {
-      case 0:
-        return `There are no items in the cart`
-      case 1:
-        return `There is one item in the cart`
-      default:
-        return `There are ${this.items.length} items in the cart`
-    }
+function countdown() {
+  let i
+  console.log("Countdown:")
+  for(i=5; i>=0; i--) { // i need to be declared here
+    setTimeout(function() {
+      console.log(i===0 ? "GO!" : i)
+    }, (5-i)*1000)
   }
 }
 
-let cart = new Cart()
-
-autorun(() => {
-  console.log(`${cart.description} ${cart.modified}`)
-})
-
-// Create the actions
-const incrementCount = action(() => {
-  cart.items.push(cart.itemCount++)
-})
-
-const addItem = action((name, quantity) => {
-  const item = cart.items.find(x => x.name === name)
-  if (item) {
-    item.quantity += 1
-  } else {
-    cart.items.push({ name, quantity })
-  }
-
-  cart.modified = new Date()
-})
-
-const removeItem = action(name => {
-  const item = cart.items.find(x => x.name === name)
-  if (item) {
-    item.quantity -= 1;
-
-    if (item.quantity <= 0) {
-      cart.items.removeItem(item)
-    }
-
-    cart.modified = new Date()
-  }
-})
-
-// Invoke actions
-console.log("Before timeout: " + new Date())
-setTimeout(addItem, 1000, 'balloons', 2)
-setTimeout(addItem, 2000, 'paint', 2)
-setTimeout(removeItem, 3000, 'paint')
+countdown()
 
 class MobX extends Component {
   state = {
